4bb2bd23655f955dc0c842de6eb7faaebbef7080
Martin Savov <Martin.Savov@vivacom.bg>
2021-01-22 16:43:53 +0200

    final?

--- src/components/TablePage.js
+++ src/components/TablePage.js
@@ -1,47 +1,137 @@
 import React from "react";
-import Table from "react-bootstrap/Table";
-import { withRouter } from "react-router-dom";
-import data from './data/test.json'
-class tablePage extends React.Component{
+import {Bar} from "react-chartjs-2";
+import data from './data/2021-01-09.json'
+
+export default class TablePage extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {data: [], setData: []}
+        this.state = {
+            GPUUti: {
+            labels: [data["GPU 0"].timestamp, data["GPU 1"].timestamp],
+            datasets: [
+                {
+                    label: 'GPU Utilization %',
+                    fill: false,
+                    lineTension: 0.5,
+                    backgroundColor: 'red',
+                    borderColor: 'red',
+                    borderWidth: 2,
+                    data: [data["GPU 0"]["GPU Utilization"].slice(0, 1), data["GPU 1"]["GPU Utilization"].slice(0, 1)]
+
+                }]},
+        GPUMemory:
+                {
+                    labels: [data["GPU 0"].timestamp, data["GPU 1"].timestamp],
+                    datasets: [
+                        {
+                    label: 'GPU Memory MiB',
+                    fill: false,
+                    lineTension: 0.5,
+                    backgroundColor: 'blue',
+                    borderColor: 'blue',
+                    borderWidth: 2,
+                    data: [data["GPU 0"]["GPU Memory"].slice(0,2),data["GPU 1"]["GPU Memory"].slice(0,2)]
+                }]},
+                CPUUti:
+                {
+                    labels: [data["GPU 0"].timestamp, data["GPU 1"].timestamp],
+                    datasets: [
+                        {
+                    label: 'CPU Utilization %',
+                    fill: false,
+                    lineTension: 0.5,
+                    backgroundColor: 'green',
+                    borderColor: 'green',
+                    borderWidth: 2,
+                    data: [data["GPU 0"]["CPU Utilization"].slice(0,1),data["GPU 1"]["CPU Utilization"].slice(0,1)]
+                }]},
+                Memory:
+                {
+                    labels: [data["GPU 0"].timestamp, data["GPU 1"].timestamp],
+                    datasets: [
+                        {
+                    label: 'Memory MiB',
+                    fill: false,
+                    lineTension: 0.5,
+                    backgroundColor: 'orange',
+                    borderColor: 'orange',
+                    borderWidth: 2,
+                    data: [data["GPU 0"]["Memory"].slice(0,4),data["GPU 1"]["Memory"].slice(0,4)]
+                }
+            ]}
+        };
     }
     render() {
         return (
             <div>
-                <h1 className="col-lg-1 col-centered">JSON reader</h1>
-                <Table className={"table-striped table-hover"} >
-                    <thead>
-                    <tr style={{background: "orange"}}>
-                        <th>Id</th>
-                        <th>First name</th>
-                        <th>Last name</th>
-                        <th>email</th>
-                        <th>gender</th>
-                        <th>ip address</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {data.map((person) => {
-                            return (
-                            <tr>
-                                <td>{person.id}</td>
-                                <td>{person.first_name}</td>
-                                <td>{person.last_name}</td>
-                                <td>{person.email}</td>
-                                <td>{person.gender}</td>
-                                <td>{person.ip_address}</td>
-                            </tr>
-                            )
-                    })}
-                    </tbody>
-                </Table>
+            <div style={{display: "table"}}>
+            <div style={{display: "table-cell", width: "770px", height: "385px"}}>
+                        <Bar
+                            data={this.state.GPUUti}
+                            options={{
+                                title: {
+                                    display: true,
+                                    text: 'GPU Utilization',
+                                    fontSize: 20,
+                                },
+                                legend: {
+                                    display: true,
+                                    position: 'right'
+                                }
+                            }}
+                            />
+            </div>
+            <div style={{display: "table-cell", width: "770px", height: "385px"}}>
+                            <Bar
+                                data={this.state.GPUMemory}
+                                options={{
+                                    title: {
+                                        display: true,
+                                        text: 'GPU Memory',
+                                        fontSize: 20,
+                                    },
+                                    legend: {
+                                        display: true,
+                                        position: 'right'
+                                    }
+                                }}
+                            />
+            </div>
+                <div style={{display: "table-cell", width: "770px", height: "385px"}}>
+                    <Bar
+                        data={this.state.CPUUti}
+                        options={{
+                            title: {
+                                display: true,
+                                text: 'CPU Utilization',
+                                fontSize: 20,
+                            },
+                            legend: {
+                                display: true,
+                                position: 'right'
+                            }
+                        }}
+                    />
+                </div>
+            </div>
+                <div style={{display: "block", width: "770px", height: "385px"}}>
+                    <Bar
+                        data={this.state.Memory}
+                        options={{
+                            title: {
+                                display: true,
+                                text: 'Memory',
+                                fontSize: 20,
+                            },
+                            legend: {
+                                display: true,
+                                position: 'right'
+                            }
+                        }}
+                    />
+                </div>
             </div>
         )
     }
 }
-export default withRouter(tablePage);
-
-
